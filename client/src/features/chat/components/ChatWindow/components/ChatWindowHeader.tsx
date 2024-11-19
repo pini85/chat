2f7ac5300b8b71b42fde17e3f6b9e9e0aa05d6f2
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2024-11-19 22:04:30 +0200

    made home page and removed old messsges from json

--- client/src/features/chat/components/ChatWindow/components/ChatWindowHeader.tsx
+++ client/src/features/chat/components/ChatWindow/components/ChatWindowHeader.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import { ChatWindowProps } from "@/features/chat/types";
+import chatRooms from "../../../utils/chatRooms";
+
 const ChatWindowHeader: React.FC<ChatWindowProps> = ({
   isMobile,
   selectedRoom,
 }) => {
+  const room = chatRooms.find((room) => room.id === selectedRoom);
+  const roomName = room ? room.name : "Select a room";
   return (
     <Typography
       variant="h5"
+      color="primary.contrastText"
       sx={{
         fontSize: isMobile ? "1rem" : "1.5rem",
         marginBottom: isMobile ? 1 : 2,
       }}
     >
-      {selectedRoom ? `Room: ${selectedRoom}` : "Select a room"}
+      {roomName}
     </Typography>
   );
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,42 @@
+import React from "react";
+import { Box, Typography, Container, Button } from "@mui/material";
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 4,
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h1" color="primary.contrastText" gutterBottom>
+          Let’s Chat
+        </Typography>
+        <Typography
+          variant="h2"
+          color="secondary.main"
+          gutterBottom
+          sx={{ fontSize: "1.5rem" }}
+        >
+          Join real-time group chats instantly!
+        </Typography>
+        <Typography
+          variant="body1"
+          color="primary.contrastText"
+          gutterBottom
+          sx={{ maxWidth: 600, margin: "0 auto" }}
+        >
+          Let’s Chat is a simple and fast group chat application where you can
+          interact in predefined chat groups, share messages, and see real-time
+          read receipts.
+        </Typography>
+      </Container>
+    </main>
   );
 };
